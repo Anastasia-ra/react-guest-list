572a58a162e48af63adde733c644034b5fe7237f
Anastasia Rambaud <anastasia.rambaud@gmail.com>
2022-02-01 18:08:52 +0100

    Update style

--- src/Style.js
+++ src/Style.js
@@ -53,35 +53,45 @@ export const buttonStyle = css`
 
 export const listStyle = css`
   list-style: none;
+  display: flex;
+  // flex-wrap: nowrap;
+  justify-content: space-between;
+  white-space: pre-wrap;
+`;
+
+export const attendingStyle = css`
+  color: rgba(190, 190, 190);
 `;
 
 export const guestStyle = css`
   display: flex;
+  min-width: 430px;
 `;
 
 export const inputStyle = css`
-  /* grid-column: 1/2;
-  grid-row: 2/3; */
+  position: absolute;
+  left: 350px;
 `;
 
 export const guestListStyle = css`
   /* grid-column: 2/3;
   grid-row: 2/3; */
+  position: absolute;
+  left: 610px;
 `;
 
 export const removeButton = css`
   display: inline-flex;
   align-items: center;
   justify-content: center;
-  background-color: red;
+  background-color: rgb(251, 215, 215);
   border: none;
   transition: background-color 0.3s ease-in 0s;
   color: rgb(255, 255, 255);
   font-weight: 600;
-  font-family: 'proxima-nova', 'Helvetica Neue', Helvetica, Arial, sans-serif !important;
   text-align: center;
   vertical-align: middle;
-  border-radius: 0.25rem;
+  border-radius: 50%;
   cursor: pointer;
   padding: 4px 6px;
   // width: auto;
@@ -89,9 +99,40 @@ export const removeButton = css`
   font-size: 0.7rem;
   letter-spacing: 0.018rem;
   line-height: 1.269rem;
-  opacity: 0.5;
+  // opacity: 0.2;
   margin-left: 10px;
+  margin-top: auto;
+  margin-bottom: auto;
   :hover {
-    background-color: rgb(139, 0, 0);
+    background-color: rgb(225, 121, 121);
+  }
+`;
+
+export const removeAllStyle = css`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgb(216, 129, 129);
+  border: none;
+  transition: background-color 0.3s ease-in 0s;
+  color: white;
+  font-weight: 600;
+  text-align: center;
+  vertical-align: middle;
+  border-radius: 0.25rem;
+  cursor: pointer;
+  padding: 4px 6px;
+  // width: auto;
+  height: 36px;
+  width: 140px;
+  font-size: 0.8rem;
+  letter-spacing: 0.018rem;
+  line-height: 1.269rem;
+  // opacity: 0.2;
+  margin-left: 10px;
+  margin-top: auto;
+  margin-bottom: auto;
+  :hover {
+    background-color: rgb(193, 79, 79);
   }
 `;
